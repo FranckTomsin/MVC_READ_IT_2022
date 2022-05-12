e237f7ef7b9dadd8fea3b0bd2f6536c295465b09
Franck Tomsin <francktomsin@gmail.com>
2022-05-12 13:35:12 +0200

    Découpage du template en partials

--- public/index.php
+++ public/index.php
@@ -2,5 +2,5 @@
 
 require_once '../core/init.php';
 require_once '../app/routers/index.php';
-require_once '../app/views/template/index.php';
+require_once '../app/views/templates/index.php';
 require_once '../core/close.php';
